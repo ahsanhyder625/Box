4c0d3c6259b6c98baed5387d9d309fec7ca4ec0b
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-12-17 23:03:59 +0530

    Updated with zindex changes

--- index.js
+++ index.js
@@ -1,24 +1,28 @@
-var box1=document.getElementById('first_div')
-box1.addEventListener('click', ()=>{
-    box1.style.zIndex=2
-})
+var box1 = document.getElementById('first_div');
+box1.addEventListener('click', () => {
+	box1.classList.toggle('toggle1');
+	box1.classList.toggle('toggle2');
+});
 
-var box2=document.getElementById('second_div')
-box2.addEventListener('click', ()=>{
-    box2.style.zIndex=2
-})
-var box3=document.getElementById('third_div')
-box3.addEventListener('click', ()=>{
-    box3.style.zIndex=2
-})
+var box2 = document.getElementById('second_div');
+box2.addEventListener('click', () => {
+	box2.classList.toggle('toggle1');
+	box2.classList.toggle('toggle2');
+});
+var box3 = document.getElementById('third_div');
+box3.addEventListener('click', () => {
+	box3.classList.toggle('toggle1');
+	box3.classList.toggle('toggle2');
+});
 
-var box4=document.getElementById('fourth_div')
-box4.addEventListener('click', ()=>{
-    box4.style.zIndex=2
-})
+var box4 = document.getElementById('fourth_div');
+box4.addEventListener('click', () => {
+	box4.classList.toggle('toggle1');
+	box4.classList.toggle('toggle2');
+});
 
-var box5=document.getElementById('fifth_div')
-box5.addEventListener('click', ()=>{
-    box5.style.zIndex=2
-})
-
+var box5 = document.getElementById('fifth_div');
+box5.addEventListener('click', () => {
+	box5.classList.toggle('toggle1');
+	box5.classList.toggle('toggle2');
+});
