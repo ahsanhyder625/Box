d99a506538046f6155203645b15e3feb37f3b658
Ahsan hyder <39864885+ahsanhyder625@users.noreply.github.com>
2020-12-17 20:00:34 +0530

    Box assignment

---
+++ index.js
@@ -0,0 +1,24 @@
+var box1=document.getElementById('first_div')
+box1.addEventListener('click', ()=>{
+    box1.style.zIndex=2
+})
+
+var box2=document.getElementById('second_div')
+box2.addEventListener('click', ()=>{
+    box2.style.zIndex=2
+})
+var box3=document.getElementById('third_div')
+box3.addEventListener('click', ()=>{
+    box3.style.zIndex=2
+})
+
+var box4=document.getElementById('fourth_div')
+box4.addEventListener('click', ()=>{
+    box4.style.zIndex=2
+})
+
+var box5=document.getElementById('fifth_div')
+box5.addEventListener('click', ()=>{
+    box5.style.zIndex=2
+})
+
